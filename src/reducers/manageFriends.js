9eb02b505ef15f48ecf4b72a2fd3c7f4e98b89c5
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-05-01 10:56:42 -0500

    commit tree 65c6e7dbd0282b749982418d3158c86ad3d8dee2

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            const newFriend = action.friend
+            return {friends: [...state.friends, newFriend]}
+        case 'REMOVE_FRIEND':
+            const new_friends = state.friends.filter(friend => friend.id !== action.id)
+            return {friends: new_friends}
+        default:
+            return state
+    }
 }
